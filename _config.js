c0a1403b9c9ea677c542695dbcd65a59d8a2a5cb
Jmaisiba <35705417+Jmaisiba@users.noreply.github.com>
2024-04-17 15:57:51 +0300

    Corrected _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,10 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://maisiba:mvmcHNB10@gallery.cwo3c4s.mongodb.net/?retryWrites=true&w=majority&appNamvmcHNB10',
-    development: 'mongodb+srv://maisiba:mvmcHNB10@gallery.cwo3c4s.mongodb.net/?retryWrites=true&w=majority&appNamvmcHNB10',
-    test: 'mongodb+srv://maisiba:mvmcHNB10@gallery.cwo3c4s.mongodb.net/?retryWrites=true&w=majority&appNamvmcHNB10',
+    production: 'mmongodb+srv://maisiba:mvmcHNB10@gallery.cwo3c4s.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    development: 'mongodb+srv://maisiba:mvmcHNB10@gallery.cwo3c4s.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
+    test: 'mmongodb+srv://maisiba:mvmcHNB10@gallery.cwo3c4s.mongodb.net/?retryWrites=true&w=majority&appName=gallery',
 }
 module.exports = config;
+
+
